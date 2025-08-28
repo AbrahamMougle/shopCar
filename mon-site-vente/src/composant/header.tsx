2163c629a4a325ff9005da547f258c4fa80206bf
AbrahamMougle <abrahammougle76@gmail.com>
2025-08-28 18:58:59 +0200

    De style au navigateur en utilisant Navlink pour indiquer que telle lien est click

--- mon-site-vente/src/composant/header.tsx
+++ mon-site-vente/src/composant/header.tsx
@@ -1,34 +1,34 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 export default function Header() {
     return <>
     <header className="bg-gradient-to-r from-orange-400 to-orange-600 shadow-md">
           <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-            <Link
+            <NavLink
               to='/'
-              className="text-3xl font-bold text-white hover:text-orange-100 transition-colors"
+             className={({isActive}:{isActive:boolean})=> isActive ? 'px-4 py-2 text-white font-bold text-2xl rounded-lg hover:bg-orange-500 transition-colors':'px-4 py-2 text-white font-medium rounded-lg hover:bg-orange-500 transition-colors'}
             >
               🚐 #VanLife
-            </Link>
+            </NavLink>
 
             <nav className="flex gap-4">
-              <Link
+              <NavLink
                 to='/vans'
-                className="px-4 py-2 text-white font-medium rounded-lg hover:bg-orange-500 transition-colors"
+             className={({isActive}:{isActive:boolean})=> isActive ? 'px-4 py-2 text-white font-bold text-2xl rounded-lg hover:bg-orange-500 transition-colors':'px-4 py-2 text-white font-medium rounded-lg hover:bg-orange-500 transition-colors'}
               >
                 Vans
-              </Link>
-              <Link
+              </NavLink>
+              <NavLink
                 to='/about'
-                className="px-4 py-2 text-white font-medium rounded-lg hover:bg-orange-500 transition-colors"
+             className={({isActive}:{isActive:boolean})=> isActive ? 'px-4 py-2 text-white font-bold text-2xl rounded-lg hover:bg-orange-500 transition-colors':'px-4 py-2 text-white font-medium rounded-lg hover:bg-orange-500 transition-colors'}
               >
                 About
-              </Link>
-              <Link
+              </NavLink>
+              <NavLink
                 to='/host'
-                className="px-4 py-2 text-white font-medium rounded-lg hover:bg-orange-500 transition-colors"
+             className={({isActive}:{isActive:boolean})=> isActive ? 'px-4 py-2 text-white font-bold text-2xl rounded-lg hover:bg-orange-500 transition-colors':'px-4 py-2 text-white font-medium rounded-lg hover:bg-orange-500 transition-colors'}
               >
                 host
-              </Link>
+              </NavLink>
             </nav>
           </div>
         </header>
