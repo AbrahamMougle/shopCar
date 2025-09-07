66da5b1ce124269b1863c903b2a103888b5c5100
AbrahamMougle <abrahammougle76@gmail.com>
2025-09-07 12:04:17 +0200

    refactorisation du code  pour eviter la redondance

--- mon-site-vente/src/composant/layoutHeader.tsx
+++ mon-site-vente/src/composant/layoutHeader.tsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom"
 import CustomNavLink from "./customnavLink"
+
 export default function LayoutHost() {
-    return <div>
-        <nav>
-            
-              <CustomNavLink to="." end>Dashboard</CustomNavLink>
-              <CustomNavLink to="review">Review</CustomNavLink>
-              <CustomNavLink to="income">Income</CustomNavLink>
-              <CustomNavLink to="hostvan">Vans</CustomNavLink>
-            </nav>
-            <Outlet />
+    const listNavigation = ['Dashboard', 'Review', 'Income', 'Vans']
+    const list = listNavigation.map((value, index) => {
+        if (value == "Dashboard") {
+            return <CustomNavLink to="." inactiveClass="text-primary-500 font-bold text-lg hover:text-primary-700 hover:scale-105 px-2 py-1 transition-all duration-200" activeClass="text-primary-500 font-bold  px-2 py-1 text-lg underline transition-all duration-200" end>{value.toLocaleUpperCase()} </CustomNavLink>
+        }
+        return <CustomNavLink to={value.toLocaleLowerCase()} inactiveClass="text-primary-500 font-bold text-lg hover:text-primary-700 hover:scale-105 px-2 py-1 transition-all duration-200" activeClass="text-primary-500 font-bold  px-2 py-1 text-lg underline transition-all duration-200"> {value.toLocaleUpperCase()} </CustomNavLink>
+    })
+
+
+    return <div className="p-2">
+        <nav> {list}</nav>
+        <Outlet />
     </div>
-    
+
 }
