d5a59b265e8392117e506eeccf7b694440e30a9f
AbrahamMougle <abrahammougle76@gmail.com>
2025-09-07 12:12:41 +0200

    refactorisation du code  et

--- mon-site-vente/src/composant/navigatorDetail.tsx
+++ mon-site-vente/src/composant/navigatorDetail.tsx
@@ -1,13 +1,14 @@
 import CustomNavLink from "./customnavLink"
 export default function NavigatorDetail() {
+    const navigationList=['Detail','Price','Photo']
+     const list = navigationList.map((value, index) => {
+            if (value == "Detail") {
+                return <CustomNavLink to="." inactiveClass="text-primary-500 font-bold text-lg hover:text-primary-700 hover:scale-105 px-2 py-1 transition-all duration-200" activeClass="text-primary-500 font-bold  px-2 py-1 text-lg underline transition-all duration-200" end>{value.toLocaleUpperCase()} </CustomNavLink>
+            }
+            return <CustomNavLink to={value.toLocaleLowerCase()} inactiveClass="text-primary-500 font-bold text-lg hover:text-primary-700 hover:scale-105 px-2 py-1 transition-all duration-200" activeClass="text-primary-500 font-bold  px-2 py-1 text-lg underline transition-all duration-200"> {value.toLocaleUpperCase()} </CustomNavLink>
+        })
     return <div>
-        <nav>
-            <CustomNavLink to="." end >Detail</CustomNavLink>
-            <CustomNavLink to="price" >Price</CustomNavLink>
-            <CustomNavLink to="photo" >Photo</CustomNavLink>
-            
-            
-        </nav>
+        <nav>{list}</nav>
     </div>
     
 }
