baf6e1f66985fa478f452ca9b12576da6f2cbe43
AbrahamMougle <abrahammougle76@gmail.com>
2025-09-07 11:02:13 +0200

    une amelioration en remplacant mon usseEffet par un loader pour ameliorer l'experience utilisateur

--- mon-site-vente/src/page/host/hostVan.tsx
+++ mon-site-vente/src/page/host/hostVan.tsx
@@ -1,5 +1,8 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
+import fetcher from "../../function/fetchVan";
+export async function loaderHost(): Promise<{ vans: Van[] }> {
+    return await fetcher('/api/vans')
+}
 
 interface Van {
     id: number;
@@ -7,18 +10,15 @@ interface Van {
     price: number;
     description: string;
     urlImage: string;
-    type: string;
+    type: "luxury" | "rugged" | "simple";
 }
-
 export default function HostVans() {
-    const [vans, setVans] = useState<Van[] | null>(null);
-
-    useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans));
-    }, []);
-
+    const vans = useLoaderData<typeof loaderHost>().vans
+    const typeColors: Record<Van["type"], string> = {
+        luxury: "bg-primary-500",
+        rugged: "bg-primary-700",
+        simple: "bg-primary-900",
+    };
     return (
         <>
             <h1 className="text-3xl font-bold">Explore Our Van option </h1>
@@ -34,12 +34,7 @@ export default function HostVans() {
                                 ${van.price} <span className="text-sm text-gray-500">/day</span>
                             </p>
                             <i
-                                className={`inline-block mt-3 px-2 py-1 text-sm rounded text-white ${van.type === "luxury"
-                                    ? "bg-yellow-500"
-                                    : van.type === "camper"
-                                        ? "bg-green-500"
-                                        : "bg-red-500"
-                                    }`}
+                                className={`inline-block mt-3 px-2 py-1 text-sm rounded text-white ${typeColors[van.type]}`}
                             >
                                 {van.type}
                             </i>
