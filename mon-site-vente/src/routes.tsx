c506e48909be70829e30ebac25bfe38374195749
AbrahamMougle <abrahammougle76@gmail.com>
2025-09-11 17:42:59 +0200

    ...

--- mon-site-vente/src/routes.tsx
+++ mon-site-vente/src/routes.tsx
@@ -9,7 +9,7 @@ import Review from "./page/host/review"
 import HostDetailInfo from "./page/hostVandetailInfo"
 import HostDetailPhoto from "./page/host/hostDetailPhoto"
 import HostVanDetail from "./page/host/hostVanDetail"
-import HostVans ,{loaderHost}  from "./page/host/hostVan"
+import HostVans, { loaderHost } from "./page/host/hostVan"
 import VanDetail, { loaderData } from "./page/VanDetail"
 import NotFound from "./page/notFound"
 import HandleErrorRouterVan from "./page/host/handleErrorRoute"
@@ -19,25 +19,26 @@ import requireAuth from "./function/requireAuth"
 
 
 
- export const routesApp = [
-  {    path: "/",
+export const routesApp = [
+  {
+    path: "/",
     element: <Layout />,
     errorElement: <HandleErrorRouterVan />,
     children: [
       { index: true, element: <Accueil />, loader: async ({ request }: { request: Request }) => { requireAuth(request) } },
       { path: "about", element: <About />, loader: async ({ request }: { request: Request }) => { requireAuth(request) } },
       { path: "connecte", element: <Contact />, action: action, },
-      { path: "register", element: <Resgister />},
+      { path: "register", element: <Resgister /> },
       { path: 'rente', element: <Vans />, loader: datafetch },
       { path: "rente/:id", element: <VanDetail />, loader: loaderData },
 
       {
         path: "myvan",
         element: <LayoutHost />,
-        children: [ 
-          { index:true ,path: "income", element: <Income /> },
+        children: [
+          { path: "income", element: <Income /> },
           { path: "review", element: <Review /> },
-          { path: "vans", element: <HostVans />,loader:loaderHost },
+          { index: true, path: "vans", element: <HostVans />, loader: loaderHost },
           {
             path: "vans/:id",
             element: <HostVanDetail />,
